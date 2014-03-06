952a7cb5c69935366a2af83dd815a146c954392d
Jag <oshcxjc@gmail.com>
2014-03-06 15:04:12 +1100

    inital commit

---
+++ src/thirdproject/Thirdproject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package thirdproject;
+
+/**
+ *
+ * @author e10505
+ */
+public class Thirdproject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Trying something here");
+    }
+    
+}
