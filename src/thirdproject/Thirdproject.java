edebfd42d5f16b9d8633ae90384101648364c182
Jag <oshcxjc@gmail.com>
2014-03-06 17:20:00 +1100

    finished for 1st release

--- src/thirdproject/Thirdproject.java
+++ src/thirdproject/Thirdproject.java
@@ -17,6 +17,7 @@ public class Thirdproject {
      */
     public static void main(String[] args) {
         System.out.println("Trying something here");
+        System.out.println("Typing something again is the change");
     }
     
 }
